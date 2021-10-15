07d33628a7f88e82e396a549dbe3146f811ecd7b
Marissa Kalkar <mkalkar@wustl.edu>
2021-10-15 00:14:15 -0500

    on part 2 #4 stuck.

--- js/activity2.js
+++ js/activity2.js
@@ -3,7 +3,7 @@ var svg = d3.select("#map-area").append("svg")
     .attr("height", 600);
 
 
-var projection = d3.mercator()
+var projection = d3.geoMercator()
     .translate([1000 / 2, 600 / 2]);
 
 
@@ -13,13 +13,15 @@ var path = d3.geoPath()
 // Load shapes of U.S. counties (TopoJSON)
 d3.json("data/world-110m.json")
     .then(function(data) {
-      console.log(data);
-// Convert TopoJSON to GeoJSON (target object = 'states')
-// var usa = topojson.feature(data, data.objects.states).features
-//
-// // Render the U.S. by using the path generator
-// svg.selectAll("path")
-//     .data(usa)
-//     .enter().append("path")
-//     .attr("d", path);
+    console.log(data);
+//Convert TopoJSON to GeoJSON (target object = 'states')
+console.log(data.objects.countries);
+var country = topojson.feature(data, data.objects.countries).features;
+
+
+// Render the U.S. by using the path generator
+svg.selectAll("path")
+    .data(country)
+    .enter().append("path")
+    .attr("d", path);
 });
